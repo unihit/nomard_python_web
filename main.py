d019cc9caa55b5da52e3593c49ec180544c3b0cc
unihit <unihit@naver.com>
2020-08-21 08:50:44 +0000

    Add indeed.py, Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&radius=25&l=%EC%84%9C%EC%9A%B8&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
+last_indeed_page = extract_indeed_pages()
 
-print(indeed_result.text)
-
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-
-pagination = indeed_soup.find("div", {"class":"pagination"})
-
-print(pagination)
+extract_indeed_jobs(last_indeed_page)
